900a946f66120a12fd81ba852dcaf20c1f45a80c
Maurício Correa <correamauricio.design@gmail.com>
2026-05-13 17:43:45 -0300

    Adiciona classe `GameCategory`

---
+++ src/GameCategory.java
@@ -0,0 +1,15 @@
+public class GameCategory {
+    private String name;
+
+    public GameCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
