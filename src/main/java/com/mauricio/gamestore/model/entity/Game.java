5beb4cbfc570ea425f3fde73fa77d7b922822c2b
Maurício Correa <correamauricio.design@gmail.com>
2026-05-13 20:20:43 -0300

    Deleta classe `GameCategory` e trata category como String

--- src/main/java/com/mauricio/gamestore/model/entity/Game.java
+++ src/main/java/com/mauricio/gamestore/model/entity/Game.java
@@ -3,10 +3,10 @@ package main.java.com.mauricio.gamestore.model.entity;
 public class Game {
     private String title;
     private String gender;
-    private GameCategory category;
+    private String category;
     private double price;
 
-    public Game(String title, String gender, GameCategory category, double price) {
+    public Game(String title, String gender, String category, double price) {
         this.title = title;
         this.gender = gender;
         this.category = category;
@@ -29,11 +29,11 @@ public class Game {
         this.gender = gender;
     }
 
-    public GameCategory getCategory() {
+    public String getCategory() {
         return category;
     }
 
-    public void setCategory(GameCategory category) {
+    public void setCategory(String category) {
         this.category = category;
     }
 
