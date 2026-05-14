36f602850706d9845a5ec6399577aaddf99c5b3e
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 12:21:23 -0300

    Adiciona listagem de categorias no `GameCategoryDAO`

--- app/src/main/java/com/mauricio/gamestore/model/dao/GameCategoryDAO.java
+++ app/src/main/java/com/mauricio/gamestore/model/dao/GameCategoryDAO.java
@@ -2,10 +2,9 @@ package com.mauricio.gamestore.model.dao;
 
 import com.mauricio.gamestore.model.entity.GameCategory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameCategoryDAO {
     private final Connection conn;
@@ -32,5 +31,24 @@ public class GameCategoryDAO {
             System.err.println(e.getMessage());
         }
         return null;
+    }
+
+    public List<GameCategory> findAll() {
+        String sql = "SELECT * FROM categoria;";
+        List<GameCategory> gameCategories = new ArrayList<>();
+
+        try (PreparedStatement stmt = this.conn.prepareStatement(sql)) {
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                gameCategories.add(new GameCategory(
+                        resultSet.getInt("id"),
+                        resultSet.getString("nome")
+                ));
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao listar as categorias: " + e.getMessage());
+        }
+        return gameCategories;
     }
 }
