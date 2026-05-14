fc79c181df9a25252ee593c4fd5237ad7cb063d5
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 15:14:15 -0300

    Adiciona `GameCategoryView`

---
+++ app/src/main/java/com/mauricio/gamestore/view/GameCategoryView.java
@@ -0,0 +1,23 @@
+package com.mauricio.gamestore.view;
+
+import com.mauricio.gamestore.controller.GameCategoryController;
+import com.mauricio.gamestore.model.entity.GameCategory;
+
+import java.util.List;
+
+public class GameCategoryView {
+    private final GameCategoryController gameCategoryController;
+
+    public GameCategoryView(GameCategoryController gameCategoryController) {
+        this.gameCategoryController = gameCategoryController;
+    }
+
+    public void displayAllCategories () {
+        List<GameCategory> allCategories = gameCategoryController.findAll();
+
+        System.out.println("\n-- TODAS AS CATEGORIAS --");
+        for (GameCategory category : allCategories) {
+            System.out.printf("%d. %s%n", category.getId(), category.getTitle());
+        }
+    }
+}
