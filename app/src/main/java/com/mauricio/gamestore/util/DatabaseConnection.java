ce9fc77a9d3c272d27ed02e3c950c52ac02e2ba6
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 00:34:56 -0300

    Cria conexão da aplicação dom o DB

--- app/src/main/java/com/mauricio/gamestore/util/DatabaseConnection.java
+++ app/src/main/java/com/mauricio/gamestore/util/DatabaseConnection.java
@@ -1,4 +1,15 @@
 package com.mauricio.gamestore.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/app_db?useTimezone=true&serverTimezone=UTC";
+    private static final String USER = "app_user";
+    private static final String PASSWORD = "app_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
