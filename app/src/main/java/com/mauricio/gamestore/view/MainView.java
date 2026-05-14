ad2f38ea521a0194c073fdf45b62d6c562282538
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 20:01:20 -0300

    Reorganiza menu principal

--- app/src/main/java/com/mauricio/gamestore/view/MainView.java
+++ app/src/main/java/com/mauricio/gamestore/view/MainView.java
@@ -8,74 +8,69 @@ public class MainView {
     public void displayMenu() {
         while (true) {
             System.out.println("\n -- MENU DO SISTEMA --");
-            System.out.println("1. Exibir Categorias de jogos");
-            System.out.println("2. Cadastrar novo Jogo");
-            System.out.println("3. Listar todos os Jogos");
+            System.out.println("1. Listar todos os Jogos");
+            System.out.println("2. Buscar Jogo por ID");
+            System.out.println("3. Cadastrar novo Jogo");
             System.out.println("4. Editar Jogo");
-            System.out.println("5. Cadastrar novo Cliente");
-            System.out.println("6. Listar todos os Clientes");
-            System.out.println("7. Buscar Cliente por ID");
+            
+            System.out.println("\n5. Listar todos os Clientes");
+            System.out.println("6. Buscar Cliente por ID");
+            System.out.println("7. Cadastrar novo Cliente");
             System.out.println("8. Editar Cliente");
-            System.out.println("9. Listar todas as Compras");
+
+            System.out.println("\n9. Listar todas as Compras");
             System.out.println("10. Buscar Compra por ID");
-            System.out.println("11. Editar Compra");
-            System.out.println("12. Registrar nova Compra");
-            System.out.println("0. Sair");
+            System.out.println("11. Registrar nova Compra");
+            System.out.println("12. Editar Compra");
+
+            System.out.println("\n0. Sair");
             System.out.print("Escolha uma opção: ");
 
             String option = scanner.nextLine();
 
             switch (option) {
                 case "1":
-                    GameCategoryView gameCategory = new GameCategoryView();
-                    gameCategory.displayAllCategories();
+                    new GameView().displayAllGames();
                     break;
                 case "2":
-                    GameView gameView = new GameView();
-                    gameView.registerGame();
+                    new GameView().findGameById();
                     break;
                 case "3":
-                    GameView listGamesView = new GameView();
-                    listGamesView.displayAllGames();
+                    new GameView().registerGame();
                     break;
                 case "4":
-                    GameView editGameView = new GameView();
-                    editGameView.editGame();
+                    new GameView().editGame();
                     break;
                 case "5":
-                    CustomerView customerRegView = new CustomerView();
-                    customerRegView.registerCustomer();
+                    new CustomerView().displayAllCustomers();
                     break;
                 case "6":
-                    CustomerView customerView = new CustomerView();
-                    customerView.displayAllCustomers();
+                    new CustomerView().findCustomerById();
                     break;
                 case "7":
-                    CustomerView searchCustomerView = new CustomerView();
-                    searchCustomerView.findCustomerById();
+                    new CustomerView().registerCustomer();
                     break;
                 case "8":
-                    CustomerView editCustomerView = new CustomerView();
-                    editCustomerView.editCustomer();
+                    new CustomerView().editCustomer();
                     break;
                 case "9":
-                    PurchaseView purchaseView = new PurchaseView();
-                    purchaseView.displayAllPurchases();
+                    new PurchaseView().displayAllPurchases();
                     break;
                 case "10":
-                    PurchaseView searchPurchaseView = new PurchaseView();
-                    searchPurchaseView.searchPurchaseById();
+                    new PurchaseView().searchPurchaseById();
                     break;
                 case "11":
-                    PurchaseView editPurchaseView = new PurchaseView();
-                    editPurchaseView.editPurchase();
+                    new PurchaseView().registerPurchase();
                     break;
                 case "12":
-                    PurchaseView registerPurchaseView = new PurchaseView();
-                    registerPurchaseView.registerPurchase();
+                    new PurchaseView().editPurchase();
                     break;
+
                 case "0":
+                    System.out.println("Saindo do sistema...");
                     System.exit(0);
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
             }
         }
     }
