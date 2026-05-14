c040d636544c2e78e92f033b291e9ea2e226902f
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 00:41:37 -0300

    Configura método para INSERT de um jogo no DB

--- app/src/main/java/com/mauricio/gamestore/model/dao/GameDAO.java
+++ app/src/main/java/com/mauricio/gamestore/model/dao/GameDAO.java
@@ -1,4 +1,29 @@
 package com.mauricio.gamestore.model.dao;
 
+import com.mauricio.gamestore.model.entity.Game;
+import com.mauricio.gamestore.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class GameDAO {
+    public boolean addGame(Game game) {
+        String sql = "INSER INTO jogo (titulo, genero, preco, categoria_id) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, game.getTitle());
+            stmt.setString(2, game.getGender());
+            stmt.setDouble(3, game.getPrice());
+            stmt.setInt(4, game.getCategory().getId());
+
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+    }
 }
