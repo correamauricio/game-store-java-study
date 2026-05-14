51546f6cd630137ddcb600b86013bac1ebd6e7f9
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 15:36:51 -0300

    Adiciona Menu principal com opção provisória de exibir categorias

--- app/src/main/java/com/mauricio/gamestore/view/MainView.java
+++ app/src/main/java/com/mauricio/gamestore/view/MainView.java
@@ -1,4 +1,27 @@
 package com.mauricio.gamestore.view;
 
+import java.util.Scanner;
+
 public class MainView {
+    Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu() {
+        while (true) {
+            System.out.println("\n -- MENU DO SISTEMA --");
+            System.out.println("1. Exibir Categorias de jogos");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":
+                    GameCategoryView gameCategory = new GameCategoryView();
+                    gameCategory.displayAllCategories();
+                    break;
+                case "0":
+                    System.exit(0);
+            }
+        }
+    }
 }
