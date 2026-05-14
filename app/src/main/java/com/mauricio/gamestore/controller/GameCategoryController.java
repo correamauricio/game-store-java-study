27306a1b712460f3efff61fc0245848c624912e0
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 14:29:56 -0300

    Adiciona `GameCategoryController`

---
+++ app/src/main/java/com/mauricio/gamestore/controller/GameCategoryController.java
@@ -0,0 +1,22 @@
+package com.mauricio.gamestore.controller;
+
+import com.mauricio.gamestore.model.entity.GameCategory;
+import com.mauricio.gamestore.model.service.GameCategoryService;
+
+import java.util.List;
+
+public class GameCategoryController {
+    private final GameCategoryService categoryService;
+
+    public GameCategoryController(GameCategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    public List<GameCategory> findAll () {
+        return categoryService.findAll();
+    }
+
+    public GameCategory findById(int id) {
+        return categoryService.findById(id);
+    }
+}
