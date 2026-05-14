abfb789cb2ed138dbfba8654b4bd207d58ab2ea9
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 15:47:37 -0300

    Instancia a Main e roda a interface

---
+++ app/src/main/java/com/mauricio/gamestore/Main.java
@@ -0,0 +1,11 @@
+package com.mauricio.gamestore;
+
+import com.mauricio.gamestore.view.MainView;
+
+public class Main {
+    public static void main(String[] args) {
+        MainView mainView = new MainView();
+
+        mainView.displayMenu();
+    }
+}
