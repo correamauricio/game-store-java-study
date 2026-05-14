637e42998c8ca655b6e3b95d5bfe454a2fd0ebe3
Maurício Correa <correamauricio.design@gmail.com>
2026-05-14 11:40:29 -0300

    Abstrai conexão com banco de dados do GameCategoryDAO para um Service

--- app/src/main/java/com/mauricio/gamestore/model/dao/GameCategoryDAO.java
+++ app/src/main/java/com/mauricio/gamestore/model/dao/GameCategoryDAO.java
@@ -1,7 +1,6 @@
 package com.mauricio.gamestore.model.dao;
 
 import com.mauricio.gamestore.model.entity.GameCategory;
-import com.mauricio.gamestore.util.DatabaseConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,12 +8,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class GameCategoryDAO {
+    private final Connection conn;
+
+    public GameCategoryDAO(Connection conn) {
+        this.conn = conn;
+    }
 
     public GameCategory findById(int id) {
         String sql = "SELECT * FROM categoria WHERE id = ?;";
 
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = this.conn.prepareStatement(sql)) {
 
             stmt.setInt(1, id);
             ResultSet resultSet = stmt.executeQuery();
---
+++ app/src/main/java/com/mauricio/gamestore/model/service/GameCategoryService.java
@@ -0,0 +1,20 @@
+package com.mauricio.gamestore.model.service;
+
+import com.mauricio.gamestore.model.dao.GameCategoryDAO;
+import com.mauricio.gamestore.model.entity.GameCategory;
+import com.mauricio.gamestore.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class GameCategoryService {
+    public GameCategory findById(int id) {
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            GameCategoryDAO dao = new GameCategoryDAO(conn);
+            return dao.findById(id);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar categoria", e);
+        }
+    }
+}
+
